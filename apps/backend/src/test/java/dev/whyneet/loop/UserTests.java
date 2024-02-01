6730cf6f0b35893a021b80aa240412bb4f7ead76
ArcSoftwareX <arc.software.x@gmail.com>
2024-02-01 17:51:59 +0300

    (backend) test: add User-Address relation test

--- apps/backend/src/test/java/dev/whyneet/loop/UserTests.java
+++ apps/backend/src/test/java/dev/whyneet/loop/UserTests.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.Optional;
 import java.util.Set;
 
 @DataJpaTest
@@ -20,16 +21,44 @@ public class UserTests {
     @Autowired
     private AddressRepository addressRepository;
 
+    private User makeTestUser() {
+        return new User("whyneet", "12345", "whyneet@example.org", "Why", "Neet", null, null);
+    }
+
+    private Address makeTestAddress() {
+        return new Address("Some first line", null, "City", "Country");
+    }
+
     @Test
-    public void canCreateUser() {
-        Address address = new Address("Some first line", null, "City", "Country");
-        User user = new User("whyneet", "12345", "whyneet@example.org", "Why", "Neet", null, Set.of(address));
+    public void canCreateUserWithAddresses() {
+        Address address = makeTestAddress();
+
+        User user = makeTestUser();
+        user.setAddresses(Set.of(address));
 
         userRepository.save(user);
         addressRepository.save(address);
 
         Assertions.assertNotNull(user.getId());
         Assertions.assertNotNull(address.getId());
-        Assertions.assertEquals(user.getAddresses(), Set.of(address));
+    }
+
+    @Test
+    public void canAccessAddressThroughRelation() {
+        Address address = makeTestAddress();
+
+        User user = makeTestUser();
+        user.setAddresses(Set.of(address));
+
+        userRepository.save(user);
+        addressRepository.save(address);
+
+        Optional<User> optionalFetchedUser = userRepository.findById(user.getId());
+
+        Assertions.assertTrue(optionalFetchedUser.isPresent());
+
+        User fetchedUser = optionalFetchedUser.get();
+
+        Assertions.assertEquals(fetchedUser.getAddresses(), Set.of(address));
     }
 }
