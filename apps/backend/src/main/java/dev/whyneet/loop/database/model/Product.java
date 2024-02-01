d20af00ef19e8a88425ebafd199b52370e3915f7
ArcSoftwareX <arc.software.x@gmail.com>
2024-02-01 17:59:49 +0300

    (backend) refactor: add constructor, getters and setters to Product

--- apps/backend/src/main/java/dev/whyneet/loop/database/model/Product.java
+++ apps/backend/src/main/java/dev/whyneet/loop/database/model/Product.java
@@ -22,4 +22,51 @@ public class Product {
 
     @ManyToOne
     private Store store;
+
+    public Product(String name, String description, ProductVariant productVariant, Store store) {
+        this.name = name;
+        this.description = description;
+        this.productVariant = productVariant;
+        this.store = store;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ProductVariant getProductVariant() {
+        return productVariant;
+    }
+
+    public void setProductVariant(ProductVariant productVariant) {
+        this.productVariant = productVariant;
+    }
+
+    public Store getStore() {
+        return store;
+    }
+
+    public void setStore(Store store) {
+        this.store = store;
+    }
 }
