d0d5e0b640deaa9ecdd2b1e039d211f0572259e2
ArcSoftwareX <arc.software.x@gmail.com>
2024-02-01 18:00:56 +0300

    (backend) refactor: add constructor, getters and setters to ProductVariant

--- apps/backend/src/main/java/dev/whyneet/loop/database/model/ProductVariant.java
+++ apps/backend/src/main/java/dev/whyneet/loop/database/model/ProductVariant.java
@@ -10,4 +10,28 @@ public class ProductVariant {
 
     @Column(nullable = false)
     private Double price;
+
+    public ProductVariant() {
+    }
+
+    public ProductVariant(String variantName, Double price) {
+        this.variantName = variantName;
+        this.price = price;
+    }
+
+    public String getVariantName() {
+        return variantName;
+    }
+
+    public void setVariantName(String variantName) {
+        this.variantName = variantName;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
 }
